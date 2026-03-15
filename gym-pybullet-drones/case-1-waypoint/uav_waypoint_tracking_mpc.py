c4246671bdd85c5d5908f797f49aa2c881e69f3e
nicewang <wangxiaonannice@gmail.com>
2026-03-15 23:43:55 +0800

    cmt-20260315-7
    for testing [todo bot]-2
    
    Signed-off-by: nicewang <wangxiaonannice@gmail.com>

--- gym-pybullet-drones/case-1-waypoint/uav_waypoint_tracking_mpc.py
+++ gym-pybullet-drones/case-1-waypoint/uav_waypoint_tracking_mpc.py
@@ -49,7 +49,10 @@ epsilon = 0.15    # Jump tolerance radius (triggers Jump when within 0.15m of th
 
 obs, info = env.reset(seed=42, options={})
 
-# todo
+"""
+* @todo [OPTIMIZATION]: Initialize the virtual target position at the drone's spawn location
+* @body Summarize this to README?
+"""
 # [OPTIMIZATION]: Initialize the virtual target position at the drone's spawn location
 virtual_target_pos = obs[0][0:3].copy() 
 
